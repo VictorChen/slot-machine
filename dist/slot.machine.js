4e3eb2cf2651a95f342be6b589c883f46383091c
Victor <itsvicc@gmail.com>
2015-03-19 01:22:43 -0400

    chore(slot.machine): create the necessary html

--- dist/slot.machine.js
+++ dist/slot.machine.js
@@ -1,19 +1,76 @@
 ;(function ($) {
   'use strict';
 
+  // Boilerplate that all slot machines will need!
+  var template = '<div class="slot-header"></div>' +
+                 '<div class="slot-main">' +
+                   '<div class="slot-bars"></div>' +
+                 '</div>' +
+                 '<div class="slot-display"></div>';
+
   $.widget('custom.slotMachine', {
 
-    //Options to be used as defaults
+    // Default options
     options: {
-      someValue: null
+      title: 'Slot Machine jQuery Plugin',
+      items: [
+        ['coffee-maker', 'coffee-filter', 'coffee-grounds'],
+        ['teapot', 'tea-strainer', 'loose-tea'],
+        ['espresso-machine', 'espresso-tamper', 'espresso-beans']
+      ]
     },
 
     _create: function () {
-      console.log('created');
+      // Perform basic validation
+      this._validateItems(this.options.items);
+      this._createSlotMachine();
     },
 
+    /**
+     * Basic validate function to make sure that the user is
+     * using the slot machine correctly!
+     * @param  {[array]} items items in the slot machine
+     */
+    _validateItems: function (items) {
+      if (!items.length) {
+        throw 'slotMachine: no items';
+      }
+      var numSlots = items[0].length;
+      for (var i=1; i<items.length; i++) {
+        if (items[i].length !== numSlots) {
+          throw 'slotMachine: items need to have the same length';
+        }
+      }
+    },
+
+    _createSlotMachine: function () {
+      // Create template
+      this.element.append(template);
+      
+      var $slotBars = this.element.find('.slot-bars');
+      var numColumns = this.options.items[0].length;
+      var numRows = this.options.items.length;
+      var $column, slotClass;
+      
+
+      this.element.find('.slot-header').append(this.options.title);
+
+      for (var i=0; i<numColumns; i++) {
+        $column = $('<div class="slot-bar"></div>');
+        for (var j=0; j<numRows; j++) {
+          slotClass = this.options.items[j][i];
+          $column.append('<div class="slot-box ' + slotClass + '"></div>');
+        }
+        $slotBars.append($column);
+      }
+    },
+
+    /**
+     * Done playing? Okay, let's destroy it!
+     */
     destroy: function () {
       console.log('destroyed');
     }
   });
+
 })(jQuery);
